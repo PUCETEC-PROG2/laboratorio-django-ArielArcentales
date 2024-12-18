cbba3ca5e0c1a752b34e751c75f01c7d3c706f64
Ariel Arcentales <amarcetales@puce.edu.ec>
2024-12-18 17:46:10 -0500

    Laboratorio 8

--- pokedex/models.py
+++ pokedex/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=40, null= False)
+    type = models.CharField(max_length=40, null= False)
+    weigth = models.DecimalField(max_digits=10,decimal_places=2)
+    heigth = models.DecimalField(max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
+    
+    

8c2f8cb8701895647d8fef81b22f9703da739ae7
Ariel Arcentales <amarcentales@puce.edu.ec>
2025-01-15 17:47:11 -0500

    Laboratorio 9 Mod

--- pokedex/urls.py
+++ pokedex/urls.py
@@ -6,6 +6,7 @@ app_name = 'pokedex'
 
 urlpatterns = [
     path("", views.index, name="index"), #pantalla de pokemons 
+    path("trainers/", views.trainers, name="trainers"),
     path("<int:pokemon_id>/", views.pokemon, name="pokemon"),
     path("trainer/<int:trainer_id>/", views.trainer_detail, name="trainer_detail"),
     path("add_pokemon/", views.add_pokemon, name="add_pokemon"),
